bba608903912b9e87ad63164a45f65ef2a1a797f
denilsonotero-lgtm <denilsonotero@gmail.com>
2026-08-15 05:23:36 -0300

    Update bingo_card.dart

--- app/lib/models/bingo_card.dart
+++ app/lib/models/bingo_card.dart
@@ -2,19 +2,33 @@ class BingoCard {
   final String id;
   final String playerId;
   final String roundId;
-  final List<List<int?>> numbers;
+  final List<List<int>> numbers;
+  final Set<int> markedNumbers;
 
   const BingoCard({
     required this.id,
     required this.playerId,
     required this.roundId,
     required this.numbers,
+    this.markedNumbers = const {},
   });
 
-  int get markedCount {
-    return numbers
-        .expand((row) => row)
-        .where((number) => number == null)
-        .length;
+  int get markedCount => markedNumbers.length;
+
+  bool isMarked(int number) {
+    return markedNumbers.contains(number);
+  }
+
+  BingoCard markNumber(int number) {
+    return BingoCard(
+      id: id,
+      playerId: playerId,
+      roundId: roundId,
+      numbers: numbers,
+      markedNumbers: {
+        ...markedNumbers,
+        number,
+      },
+    );
   }
 }
