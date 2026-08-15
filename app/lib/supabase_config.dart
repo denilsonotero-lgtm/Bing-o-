e75abfbfb85bba11371908dc1542bc7dbca62ed0
denilsonotero-lgtm <denilsonotero@gmail.com>
2026-08-15 12:14:01 -0300

    Update supabase_config.dart

--- app/lib/supabase_config.dart
+++ app/lib/supabase_config.dart
@@ -1,4 +1,4 @@
 class SupabaseConfig {
-  static const String url = 'COLE_AQUI_A_URL_DO_SEU_SUPABASE';
-  static const String anonKey = 'COLE_AQUI_A_CHAVE_PUBLICAVEL';
+  static const String url = 'https://zfwwxnrbxbauiomoqsim.supabase.co';
+  static const String anonKey = 'sb_publishable_aAvSRk_vCpe3MmdS_hO92w_bMKpDjsw';
 }
